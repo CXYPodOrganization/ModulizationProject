3355c57be4fb22ddc616c85b6e506ba1ea37c38b
Caoxy <stermop@163.com>
2018-05-24 13:47:39 +0800

    Initial Commit

---
+++ ModulizationProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ModulizationProject
+//
+//  Created by 曹雪莹 on 2018/5/24.
+//  Copyright © 2018年 曹雪莹. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ModulizationProject/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ModulizationProject
+//
+//  Created by 曹雪莹 on 2018/5/24.
+//  Copyright © 2018年 曹雪莹. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+
+@end
